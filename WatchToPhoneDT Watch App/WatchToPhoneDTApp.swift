366d479f9705a657039fdd5dfce45c24b0f26841
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-07-24 21:52:13 +0600

    Initial Commit

---
+++ WatchToPhoneDT Watch App/WatchToPhoneDTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchToPhoneDTApp.swift
+//  WatchToPhoneDT Watch App
+//
+//  Created by Joynal Abedin on 24/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchToPhoneDT_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchToPhoneDT/WatchToPhoneDTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchToPhoneDTApp.swift
+//  WatchToPhoneDT
+//
+//  Created by Joynal Abedin on 24/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchToPhoneDTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
